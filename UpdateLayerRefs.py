ed459e2431b99f492eecdea80cb8331c61682762
Jeremy Swagger <jeremy.swagger@gmail.com>
2017-08-14 11:52:03 -0500

    Adds option to overwrite an existing MXD

--- UpdateLayerRefs.py
+++ UpdateLayerRefs.py
@@ -46,19 +46,30 @@ def update_mxd(in_mxd, out_mxd, out_db, out_db_type, in_prefix, out_prefix):
         in_fc = lyr.datasetName
         out_fc_sanitized = re.sub(in_prefix, out_prefix, in_fc)
         lyr.replaceDataSource(out_db, out_db_type, out_fc_sanitized, False)
-    in_mxd.saveACopy(out_mxd)
+    if out_mxd:
+        in_mxd.saveACopy(out_mxd)
+        print "New MXDs is ready"
+    else:
+        in_mxd.save()
+        print "MXDs have been updated"
+
+
+def set_out_mxds(config_out_mxd):
+    if config_out_mxd:
+        config_out_mxd.encode('utf-8')
+    return config_out_mxd
 
 
 def main():
     config_file = read_config_file()
     in_mxd = arcpy.mapping.MapDocument(config_file.get("in_mxd").encode('utf-8'))
-    out_mxd = config_file.get("out_mxd").encode('utf-8')
+    out_mxd = set_out_mxds(config_file.get("out_mxd"))
     out_db = config_file.get("out_db").encode('utf-8')
     out_db_type = config_file.get("out_db_type").encode('utf-8')
     in_prefix = config_file.get("in_prefix").encode('utf-8')
     out_prefix = config_file.get("out_prefix").encode('utf-8')
     update_mxd(in_mxd, out_mxd, out_db, out_db_type, in_prefix, out_prefix)
-    print "New MXD is ready"
+
 
 
 main()
